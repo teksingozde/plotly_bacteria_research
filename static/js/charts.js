70c496f10d49617cc0a2c6d82a832dd69cfd6b35
Guner Gozde Teksin Kilic <ggteksin@gmail.com>
2022-12-08 14:18:43 -0600

    Update charts.js

--- static/js/charts.js
+++ static/js/charts.js
@@ -141,16 +141,16 @@ function init() {
   
       // 2. Create a variable that holds the first sample in the metadata array.
       var result = resultArray[0];
-      console.log(result.wfreq)
   
       // 3. Create a variable that holds the washing frequency.
-      var washFreq = parseInt(result.wfreq);
+      var freq = parseFloat(result.wfreq);
   
       // 4. Create the trace for the gauge chart.
       var gaugeData = [{
         title: {text: "Belly Button Washing Frequency<br><span>Scrubs per Week</span>"},
         type: "indicator",
         mode: "gauge+number",
+        value: freq,
         gauge: {
           axis: {range: [null, 10]},
           bar: {color: "black"},
@@ -166,7 +166,7 @@ function init() {
       
       // 5. Create the layout for the gauge chart.
       var gaugeLayout = {
-        height: 500,
+        height: 400,
         width: 500, 
         margin: { t: 0, b: 0 }
       };
